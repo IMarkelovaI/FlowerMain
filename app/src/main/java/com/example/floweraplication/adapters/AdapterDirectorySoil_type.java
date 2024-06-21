2efa1519155155c28bad19caa6a54149cb7044e1
Kate <markelova-kat@mail.ru>
2024-06-21 15:10:36 +0700

    Релизная версия 0.11

--- app/src/main/java/com/example/floweraplication/adapters/AdapterDirectorySoil_type.java
+++ app/src/main/java/com/example/floweraplication/adapters/AdapterDirectorySoil_type.java
@@ -42,6 +42,23 @@ public class AdapterDirectorySoil_type extends RecyclerView.Adapter<AdapterDirec
         String name = model.getName();
         String description = model.getDescription();
         holder.categoryTv.setText(name);
+        String s;
+        switch (name){
+            case ("Верховный торф"):
+                s="https://goldwins.ru/upload/medialibrary/cab/2_min.jpg";
+                break;
+            case("Низинный торф"):
+                s="https://www.mirmulchi.ru/images/2019/12/20/nizinnyj-torf-2.jpg";
+                break;
+            case("Биогумус"):
+                s="https://progress39.ru/uploads/product/160954700/160954717/biokompost_2020-01-29_16-30-42.jpg";
+                break;
+            case("Дерновая земля"):
+                s="https://gardennews.ru/wp-content/uploads/2019/09/011-143-900x400.jpg";
+                break;
+            default:
+                s="https://firebasestorage.googleapis.com/v0/b/floweraplication.appspot.com/o/images%2F2024_03_24_04_03_25.png?alt=media&token=572e21d3-66f5-4ed4-96ab-5b4ecc619e6a";
+        }
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,7 +67,7 @@ public class AdapterDirectorySoil_type extends RecyclerView.Adapter<AdapterDirec
                 intent.putExtra("id", id);
                 intent.putExtra("name", name);
                 intent.putExtra("description",description);
-                String picture = "https://firebasestorage.googleapis.com/v0/b/floweraplication.appspot.com/o/images%2F2024_03_24_04_03_25.png?alt=media&token=572e21d3-66f5-4ed4-96ab-5b4ecc619e6a";
+                String picture = s;
                 intent.putExtra("picture", picture);
                 context.startActivity(intent);
             }
